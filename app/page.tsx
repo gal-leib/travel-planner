03ccdbf93b11e81d2e371ba73a7c6612a4f585c9
Gal Leibovici <gal.leibo@gmail.com>
2026-02-15 22:37:48 +0200

    feat: add Speed Insights integration for performance monitoring

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { ItineraryPage } from "@/components/itinerary/itinerary-page";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Page() {
   return <ItineraryPage />;
